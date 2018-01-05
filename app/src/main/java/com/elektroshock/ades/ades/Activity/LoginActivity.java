5bb44243bc2d38f713453799be933094afb30915
irawan09 <arykurniadiirawan@yahoo.co.id>
2018-01-05 20:56:42 +0800

    tambah logo

--- app/src/main/java/com/elektroshock/ades/ades/Activity/LoginActivity.java
+++ app/src/main/java/com/elektroshock/ades/ades/Activity/LoginActivity.java
@@ -1,17 +1,35 @@
 package com.elektroshock.ades.ades.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import com.elektroshock.ades.ades.R;
 
 public class LoginActivity extends Activity {
+
+    Button masuk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
+        masuk = (Button) findViewById(R.id.submit1);
+
+        masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(LoginActivity.this, VideoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
